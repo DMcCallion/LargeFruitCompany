ed65f1537fb259559e581848429c8d5cb094c8e6
DMcCallion <danielmccallion04@gmail.com>
2023-10-09 10:36:18 +0100

    Added README

--- src/main/java/org/example/Basket.java
+++ src/main/java/org/example/Basket.java
@@ -1,2 +1,4 @@
-package org.example;public class Basket {
+package org.example;
+
+public class Basket {
 }
--- src/main/java/org/example/Fruit.java
+++ src/main/java/org/example/Fruit.java
@@ -1,2 +1,4 @@
-package org.example;public class Fruit {
+package org.example;
+
+public class Fruit {
 }
